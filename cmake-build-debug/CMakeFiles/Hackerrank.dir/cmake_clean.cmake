7695055bd00dfdb6a47d56d070c15a046ee9b3ff
bindu <bindu@Rajeshs-MacBook-Pro.local>
2020-05-07 21:15:34 +0545

    final solution

---
+++ cmake-build-debug/CMakeFiles/Hackerrank.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hackerrank.dir/Day_1.c.o"
+  "CMakeFiles/Hackerrank.dir/main.c.o"
+  "Hackerrank"
+  "Hackerrank.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Hackerrank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
